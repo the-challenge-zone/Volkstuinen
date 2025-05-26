fd9d958b441c4871a45bfe05a1b1b907d3a16c88
thomas200499 <Thomas.criekingen@gmail.com>
2025-05-26 11:08:50 +0200

    commit

--- Frontend/Gedeeld/GebruikerInfo.php
+++ Frontend/Gedeeld/GebruikerInfo.php
@@ -63,56 +63,7 @@ switch ($_SESSION['user_type'])
 <div class="header">
     VOLKSTUIN VERENIGING SITTARD
 </div>
-<?php
 
-// if (!isset($_SESSION['user_id'])) {
-//     echo "Sessie niet gevonden, probeer opnieuw in te loggen.";
-//     exit();
-// }
-                
-//     require_once '../../Backend/DatabaseContext/database.php'; // Zorg ervoor dat dit het juiste pad is naar jouw databasebestand
-    
-//     if (isset($_POST['checkUser'])) {
-//         if (isset($_SESSION['user_id'])) {
-//             try {
-//                 $conn = Database::GetConnection();
-                
-//                 // Haal usertype op uit database
-//                 $user_id = $_SESSION['user_id'];
-//                 $sql = "SELECT UserType FROM users WHERE id = :id";
-//                 $stmt = $conn->prepare($sql);
-//                 $stmt->bindParam(":id", $user_id, PDO::PARAM_INT);
-//                 $stmt->execute();
-//                 $user = $stmt->fetch();
-                
-//                 if ($user) {
-//                     $user_type = $user['UserType'];
-                    
-//                     // Stuur door op basis van usertype
-//                     switch ($user_type) {
-//                         case 1:
-//                             header("Location: ../Bestuurder/dashboard.php");
-//                             exit();
-//                         case 2:
-//                             header("Location: ../Beheerder/dashboard.php");                                  Ik snap het niet helemaal
-//                             exit();
-//                         case 3:
-//                             header("Location: ../Deelnamer/dashboard.php");
-//                             exit();
-//                         default:
-//                             echo "Ongeldig usertype.";
-//                     }
-//                 } else {
-//                     echo "Usertype niet gevonden.";
-//                 }
-//             } catch (Exception $e) {
-//                 echo "Fout bij ophalen usertype: " . htmlspecialchars($e->getMessage());
-//             }
-//         } else {
-//             echo "Geen gebruiker ingelogd.";
-//         }
-//     }
-//     ?>
 
     
 
